f5b03654cc39cc83ab6ea80e44ce02e96e5495b3
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-12-19 11:39:14 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment_7._2._3
+{
+    internal class Program
+    {
+        static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            return String.Concat(s.OrderBy(c => c)) == String.Concat(t.OrderBy(c => c));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAnagram("anagram", "nagaram")); 
+            Console.WriteLine(IsAnagram("rat", "car"));         
+        }
+    }
+}
